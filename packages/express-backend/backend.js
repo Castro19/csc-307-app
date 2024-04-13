63fcc32a2947e9ced82f17936c2aefc0424e559b
Castro19 <ccastroo@calpoly.edu>
2024-04-13 14:38:08 -0700

    Creating an endpoint for /users/:id
    This endpoint will return the object based on the unique identifier given in the id portion of the url endpoint

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -37,7 +37,7 @@ const users = {
 };
 
 const findUserByName = (name) => {
-  return users["users_list"].filter((users) => users.name === name);
+  return users["users_list"].filter((user) => user.name === name);
 };
 app.get("/users", (req, res) => {
   const name = req.query.name;
@@ -47,6 +47,21 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+const findUserById = (id) => {
+  return users["users_list"].find((user) => user.id === id);
+};
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findUserById(id);
+
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
